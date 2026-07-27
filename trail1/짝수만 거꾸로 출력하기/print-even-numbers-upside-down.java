ecf188809ef11999f41ea36fa6a52e609e4fabf6
Do Hyun <52389219+JustDoJeon@users.noreply.github.com>
2026-07-27 22:25:35 +0900

    [Passed] 짝수만 거꾸로 출력하기 (95ms, 9MB)

--- trail1/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.java
+++ trail1/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.java
@@ -1,34 +1,27 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int ans = 0;
+        int n = Integer.parseInt(br.readLine());
+
+        String[] str = br.readLine().split(" ");
+
+        int[] arr = new int[n];
         int idx = 0;
 
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(str[i]);
 
-            String[] str = br.readLine().split(" ");
-            for (int j = 0; j < str.length; j++) {
-                int score = Integer.parseInt(str[j]);
-                if (score % 2 == 0) {
-                    arr[idx] = score;
-                    idx++;
-                }
+            if (num % 2 == 0) {
+                arr[idx++] = num;
             }
-
-        }
-        for(int i=arr.length-1; i>=0; i--){
-            System.out.print(arr[i] +" ");
         }
 
+        for (int i = idx - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
