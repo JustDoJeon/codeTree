dbc95be360c12b9c20896bda58597be66fc5917a
Do Hyun <52389219+JustDoJeon@users.noreply.github.com>
2026-07-27 23:30:03 +0900

    [Runtime Error] 배열의 특정 위치 찾기 (93ms, 9MB)

--- trail1/배열의 특정 위치 찾기/find-specific-location-fo-array.java
+++ trail1/배열의 특정 위치 찾기/find-specific-location-fo-array.java
@@ -6,18 +6,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] str = new String[10];
+        String[] str = new String[11];
         int ans1 =0;
         int ans2 = 0;
         int cnt = 0;
         double avg = 0.0;
         str = br.readLine().split(" ");
 
-        for(int i=0; i<str.length; i++){
-            if((i+1)%2 ==0 ){
+        for(int i=1; i<=str.length; i++){
+            if(i%2 ==0 ){
                 ans1 += Integer.parseInt(str[i]);
             }
-            if((i+1)%3 ==0 ){
+            if(i%3 ==0 ){
                 ans2 += Integer.parseInt(str[i]);
                 cnt++;
             }
