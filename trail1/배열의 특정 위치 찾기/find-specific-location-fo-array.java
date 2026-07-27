44be92cb01727ed239cc05ab98029366ad6bb75f
Do Hyun <52389219+JustDoJeon@users.noreply.github.com>
2026-07-27 22:58:06 +0900

    [Wrong Answer] 배열의 특정 위치 찾기 (100ms, 9MB)

---
+++ trail1/배열의 특정 위치 찾기/find-specific-location-fo-array.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str = new String[10];
+        int ans1 =0;
+        int ans2 = 0;
+        int cnt = 0;
+        double avg = 0.0;
+        str = br.readLine().split(" ");
+
+        for(int i=0; i<str.length; i++){
+            if((i+1)%2 ==0 ){
+                ans1 += Integer.parseInt(str[i]);
+            }
+            if((i+1)%3 ==0 ){
+                ans2 += Integer.parseInt(str[i]);
+                cnt++;
+            }
+        }
+
+        avg  = (double) ans2 / cnt ; 
+
+        System.out.print(ans1 + " " + avg);
+
+
+    }
+
+}
