09d1bc825ddae20f9190694b6c283ddd484dea44
Do Hyun <52389219+JustDoJeon@users.noreply.github.com>
2026-08-07 01:03:06 +0900

    [Passed] N개의 정수 중 최대 2개 (87ms, 9MB)

---
+++ trail1/N개의 정수 중 최대 2개/two-max-of-n-num.java
@@ -0,0 +1,38 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[n - 1] +" " + arr[n - 2]);
+
+
+
+
+
+
+
+    }
+
+
+}
