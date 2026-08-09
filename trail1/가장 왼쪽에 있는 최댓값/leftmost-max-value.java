1b454bd9ae28a12bc30a65d2136172ea8b83ee75
Do Hyun <52389219+JustDoJeon@users.noreply.github.com>
2026-08-09 18:52:54 +0900

    [Wrong Answer] 가장 왼쪽에 있는 최댓값 (91ms, 9MB)

---
+++ trail1/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -0,0 +1,49 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+
+        StringBuffer sb = new StringBuffer();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n;
+        int[] a = new int[1000];
+
+        n = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int prevMaxIdx = n;
+
+        while (true) {
+            int maxIdx = 0;
+
+            for (int i = 1; i < prevMaxIdx; i++) {
+                if (a[i] > a[maxIdx])
+                    maxIdx = i;
+            }
+
+            System.out.println((maxIdx + 1) + " ");
+
+            if (maxIdx == 0) {
+                break;
+            }
+
+            prevMaxIdx = maxIdx;
+
+        }
+    }
+}
