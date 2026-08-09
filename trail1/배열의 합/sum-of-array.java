fd26fd1c80ff8a53d1edf5c15d6a7aec86d32294
Do Hyun <52389219+JustDoJeon@users.noreply.github.com>
2026-08-09 19:11:06 +0900

    [Passed] 배열의 합 (90ms, 9MB)

---
+++ trail1/배열의 합/sum-of-array.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+
+        StringBuffer sb = new StringBuffer();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int[][] arr = new int[4][4];
+
+        for(int i =0; i<4; i++ ) {
+        StringTokenizer   st = new StringTokenizer(br.readLine());
+            for(int j=0; j<4; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        int sum;
+        for(int i =0; i<4; i++ ) {
+            sum = 0 ;
+            for(int j=0; j<4; j++) {
+                sum+=arr[i][j];
+
+            }
+            System.out.println(sum);
+        }
+
+
+    }
+}
