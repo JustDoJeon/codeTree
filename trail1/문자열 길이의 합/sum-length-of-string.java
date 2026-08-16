b2ddd9960b9982685117f1c26e52513c2154b738
Do Hyun <52389219+JustDoJeon@users.noreply.github.com>
2026-08-16 16:55:05 +0900

    [Wrong Answer] 문자열 길이의 합 (93ms, 9MB)

--- trail1/문자열 길이의 합/sum-length-of-string.java
+++ trail1/문자열 길이의 합/sum-length-of-string.java
@@ -8,26 +8,27 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-        int len = 0;
-        int count = 0;
+        int totalLength = 0;
+        int aCount = 0;
 
         for (int i = 0; i < n; i++) {
 
             String str = br.readLine();
 
             // 모든 문자열 길이의 합
-            len += str.length();
+            totalLength += str.length();
 
-            // 첫 번째 문자열에서만 a 개수 세기
+            // 첫 번째 문자열일 때만 'a' 개수 세기
             if (i == 0) {
                 for (int j = 0; j < str.length(); j++) {
+
                     if (str.charAt(j) == 'a') {
-                        count++;
+                        aCount++;
                     }
                 }
             }
         }
 
-        System.out.println(len + " " + count);
+        System.out.println(totalLength + " " + aCount);
     }
 }
