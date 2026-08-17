c58adcb422a84c96819dde9c89a37d0209f63610
Do Hyun <52389219+JustDoJeon@users.noreply.github.com>
2026-08-17 21:01:14 +0900

    [Passed] 단어로 구분하기 (99ms, 16MB)

---
+++ trail1/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,25 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+
+        String s = br.readLine();
+
+        String[] str = s.split(" ");
+        
+        for(int i=0; i<str.length; i++){
+            System.out.println(str[i]);
+        }
+        
+
+
+    }
+
+}
