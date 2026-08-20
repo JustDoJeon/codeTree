8537d27b8fc72977ab4f76019b837d9549c823fb
Do Hyun <52389219+JustDoJeon@users.noreply.github.com>
2026-08-21 01:00:55 +0900

    [Passed] 문자열 전부 붙이기 (88ms, 9MB)

---
+++ trail1/문자열 전부 붙이기/paste-all-string.java
@@ -0,0 +1,26 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+    String ans = "";
+        for(int i = 0; i < n; i++){
+            String s = br.readLine();
+            ans += s;
+        }
+
+        System.out.println(ans  );
+
+
+
+    }
+
+}
