779afc36a9d36795ac4de43a965995e6c5ffa841
Do Hyun <52389219+JustDoJeon@users.noreply.github.com>
2026-08-26 22:55:34 +0900

    [Passed] 반복 출력하기 (122ms, 10MB)

---
+++ trail2/반복 출력하기/repeated-output.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("12345^&*()_");
+
+        }
+
+    }
+
+
+}
+
+    
